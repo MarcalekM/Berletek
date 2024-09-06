0d9f5b3c59ec6ff7eb241e7632fde8da933e2fe9
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2024-09-06 09:30:42 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bérletek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Utas> utasok = new();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\utasadat.txt",
+                encoding: Encoding.UTF8);
+            while (!sr.EndOfStream) utasok.Add(new(sr.ReadLine()));
+            int f3 = utasok.Count();
+        }
+    }
+}
---
+++ Utas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bérletek
+{
+    internal class Utas
+    {
+        string felszallasHelye { get; set; }
+        string datum {  get; set; }
+        string azonosito { get; set; }
+        string tipus {  get; set; }
+        string ervenyesseg { get; set; }
+
+        public Utas(string sor)
+        {
+            var v = sor.Split(' ').ToList();
+            felszallasHelye = v[0];
+            datum = v[1];
+            azonosito = v[2];
+            tipus = v[3];
+            ervenyesseg = v[4];
+        }
+    }
+}
